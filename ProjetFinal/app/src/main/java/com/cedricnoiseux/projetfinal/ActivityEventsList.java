4ddc3f2bdb32bcb2e04d1d5a60e3724848a6069e
Cedric Noiseux <noiseux1523@hotmail.com>
2016-04-07 17:45:12 -0400

    Done

--- ProjetFinal/app/src/main/java/com/cedricnoiseux/projetfinal/ActivityEventsList.java
+++ ProjetFinal/app/src/main/java/com/cedricnoiseux/projetfinal/ActivityEventsList.java
@@ -1,6 +1,7 @@
 package com.cedricnoiseux.projetfinal;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
@@ -8,7 +9,10 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
@@ -19,6 +23,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.LocationListener;
 
 import java.text.SimpleDateFormat;
 import java.util.LinkedList;
@@ -29,26 +36,26 @@ import java.util.List;
 // le compas disparait et les events re-apparaissent avec getAllEvents. À voir, on veut pointer vers les events ou
 // juste le nord?
 
-public class ActivityEventsList extends AppCompatActivity implements SensorEventListener {
+public class ActivityEventsList extends AppCompatActivity implements
+        SensorEventListener,
+        android.location.LocationListener{
+
     private TextView mMenu;
+    private TextView mDismiss;
     private ScrollView mScroll;
     private LinearLayout mList;
     private RelativeLayout mMain;
     private ImageView mPointer;
-    private TextView tvHeading;
+    private Boolean compassOn = false;
 
     private SensorManager mSensorManager;
     private Sensor mCompass;
-    private Sensor mAccelerometer;
-    private Sensor mMagnetometer;
-    private Sensor mMagnet;
-    private float[] mLastAccelerometer = new float[3];
-    private float[] mLastMagnetometer = new float[3];
-    private boolean mLastAccelerometerSet = false;
-    private boolean mLastMagnetometerSet = false;
-    private float[] mR = new float[9];
-    private float[] mOrientation = new float[3];
     private float mCurrentDegree = 0f;
+    private Event selectedEvent;
+    private final User user = new User("", 0, 0);
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +65,7 @@ public class ActivityEventsList extends AppCompatActivity implements SensorEvent
         Intent intent = getIntent();
         Bundle extras = intent.getExtras();
         String username = extras.getString("user");
-        User user = new User(username, 0, 0);
+        user.email = username;
 
         mMenu = (TextView) findViewById(R.id.menu);
         mMenu.setOnClickListener(new View.OnClickListener() {
@@ -67,26 +74,34 @@ public class ActivityEventsList extends AppCompatActivity implements SensorEvent
                 goToMenu();
             }
         });
+        mDismiss = (TextView) findViewById(R.id.dismiss);
+        mDismiss.setVisibility(View.GONE);
+        mDismiss.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    getEvents(ActivityEventsList.this, user);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                mDismiss.setVisibility(View.GONE);
+                mPointer.setImageResource(0);
+                mPointer.setVisibility(View.GONE);
+                compassOn = false;
+            }
+        });
         mScroll = (ScrollView) findViewById(R.id.scroll);
         mList = (LinearLayout) findViewById(R.id.list);
         mMain = (RelativeLayout) findViewById(R.id.main);
         mPointer = (ImageView) findViewById(R.id.imageDisplay);
-        tvHeading = (TextView) findViewById(R.id.tvHeading);
+        mPointer.setImageResource(R.drawable.pointer);
+        mPointer.setVisibility(View.GONE);
 
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mCompass = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        mMagnet = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        List<Sensor> msensorlist = mSensorManager.getSensorList(Sensor.TYPE_ALL);
-        String sSensList = new String("");
-        Sensor tmp;
-        int x,i;
-        for (i=0;i<msensorlist.size();i++){
-            tmp = msensorlist.get(i);
-            sSensList = " "+sSensList+tmp.getName(); // Add the sensor name to the string of sensors available
-        }
-        tvHeading.setText(sSensList);
-//        mMagnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
 
         // Search and show all events
         try {
@@ -152,20 +167,56 @@ public class ActivityEventsList extends AppCompatActivity implements SensorEvent
             mOutput.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ActivityEventsList.this);
+                    builder.setTitle("Choose an option");
+                    builder.setItems(new CharSequence[]{"Join Event", "Leave Event", "Show Direction"},
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    // The 'which' argument contains the index position
+                                    // of the selected item
+                                    switch (which) {
+                                        case 0:
+                                            if (mParticipation.getCurrentTextColor() == Color.RED) {
+                                                SqlUtility.addParticipation(user, event);
+                                                mParticipation.setTextColor(Color.GREEN);
+                                                mParticipation.setText("Attending - Click to interact");
+                                                mOutput.setBackgroundResource(R.drawable.back_green);
+                                            } else {
+                                                Toast.makeText(ActivityEventsList.this,
+                                                        "You are already attending this event", Toast.LENGTH_LONG).show();
+                                            }
+                                            break;
+                                        case 1:
+                                            if (mParticipation.getCurrentTextColor() == Color.GREEN) {
+                                                SqlUtility.removeParticipation(user, event);
+                                                mParticipation.setTextColor(Color.RED);
+                                                mParticipation.setText("Not Attending - Click to interact");
+                                                mOutput.setBackgroundResource(R.drawable.back_red);
+                                            } else {
+                                                Toast.makeText(ActivityEventsList.this,
+                                                        "You are already not attending this event", Toast.LENGTH_LONG).show();
+                                            }
+                                            break;
+                                        case 2:
+                                            if (user.lastY == 0 && user.lastX == 0) {
+                                                Toast.makeText(ActivityEventsList.this,
+                                                        "Wait until your device is located", Toast.LENGTH_LONG).show();
+                                            } else {
+                                                mList.removeAllViews();
+                                                mDismiss.setVisibility(View.VISIBLE);
+                                                mPointer.setImageResource(R.drawable.pointer);
+                                                mPointer.setVisibility(View.VISIBLE);
+                                                selectedEvent = event;
+                                                compassOn = true;
 
+                                            }
+                                            break;
+                                    }
+                                }
+                            }
 
-
-//                    if (mParticipation.getCurrentTextColor() == Color.RED) {
-//                        SqlUtility.addParticipation(user, event);
-//                        mParticipation.setTextColor(Color.GREEN);
-//                        mParticipation.setText("Attending - Click to interact");
-//                        mOutput.setBackgroundResource(R.drawable.back_green);
-//                    } else {
-//                        SqlUtility.removeParticipation(user, event);
-//                        mParticipation.setTextColor(Color.RED);
-//                        mParticipation.setText("Not Attending - Click to interact");
-//                        mOutput.setBackgroundResource(R.drawable.back_red);
-//                    }
+                    );
+                    builder.create().show();
                 }
             });
 
@@ -177,73 +228,88 @@ public class ActivityEventsList extends AppCompatActivity implements SensorEvent
 
     protected void onResume() {
         super.onResume();
-//      mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
-//      mSensorManager.registerListener(this, mMagnetometer, SensorManager.SENSOR_DELAY_GAME);
-//      mPointer.setImageResource(R.drawable.pointer);
         mSensorManager.registerListener(this, mCompass, SensorManager.SENSOR_DELAY_GAME);
-        mPointer.setImageResource(R.drawable.pointer);
     }
 
     protected void onPause() {
         super.onPause();
-//      mSensorManager.unregisterListener(this, mAccelerometer);
-//      mSensorManager.unregisterListener(this, mMagnetometer);
-        mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this, mCompass);
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        // get the angle around the z-axis rotated
-        float degree = Math.round(event.values[0]);
+        if (compassOn) {
+            // get the angle around the z-axis rotated
+            float degree = Math.round(event.values[0]);
 
-        tvHeading.setText("Heading: " + Float.toString(degree) + " degrees");
+            double eventX = selectedEvent.locY;
+            double eventY = selectedEvent.locX;
+            double userX = user.lastY;
+            double userY = user.lastX;
+            double[] vectorUserEvent = new double[]{eventX-userX, eventY-userY};
+            double[] vectorNorth = new double[]{0, 1};
+            double cos = (((vectorUserEvent[0]*vectorNorth[0]) + (vectorUserEvent[1]*vectorNorth[1])) /
+                            (Math.sqrt(vectorUserEvent[0]*vectorUserEvent[0] + vectorUserEvent[1]*vectorUserEvent[1]) *
+                            Math.sqrt(vectorNorth[0]*vectorNorth[0] + vectorNorth[1]*vectorNorth[1])));
+            double angleWithNorth = 0;
+            if (vectorUserEvent[0] > 0 && vectorUserEvent[1] > 0) {
+                angleWithNorth = 360 - Math.toDegrees(Math.acos(cos));
+            }
+            if (vectorUserEvent[0] < 0 && vectorUserEvent[1] > 0) {
+                angleWithNorth = Math.toDegrees(Math.acos(cos));
+            }
+            if (vectorUserEvent[0] < 0 && vectorUserEvent[1] < 0) {
+                angleWithNorth = Math.toDegrees(Math.acos(cos));
+            }
+            if (vectorUserEvent[0] > 0 && vectorUserEvent[1] < 0) {
+                angleWithNorth = 360 - Math.toDegrees(Math.acos(cos));
+            }
 
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                mCurrentDegree,
-                -degree,
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
+            float mEventDegree = (float) (-angleWithNorth + mCurrentDegree);
 
-        // how long the animation will take place
-        ra.setDuration(210);
-
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
-
-        // Start the animation
-        mPointer.startAnimation(ra);
-        mCurrentDegree = -degree;
-
-//        if (event.sensor == mAccelerometer) {
-//            System.arraycopy(event.values, 0, mLastAccelerometer, 0, event.values.length);
-//            mLastAccelerometerSet = true;
-//        }
-//        if (event.sensor == mMagnetometer) {
-//            System.arraycopy(event.values, 0, mLastMagnetometer, 0, event.values.length);
-//            mLastMagnetometerSet = true;
-//        }
-//        if (mLastAccelerometerSet && mLastMagnetometerSet) {
-//            SensorManager.getRotationMatrix(mR, null, mLastAccelerometer, mLastMagnetometer);
-//            SensorManager.getOrientation(mR, mOrientation);
-//            float azimuthInRadians = mOrientation[0];
-//            float azimuthInDegress = (float)(Math.toDegrees(azimuthInRadians)+360)%360;
-//            RotateAnimation ra = new RotateAnimation(
-//                    mCurrentDegree,
-//                    -azimuthInDegress,
-//                    Animation.RELATIVE_TO_SELF, 0.5f,
-//                    Animation.RELATIVE_TO_SELF, 0.5f);
-//
-//            ra.setDuration(250);
-//            ra.setFillAfter(true);
-//            mPointer.startAnimation(ra);
-//            mCurrentDegree = -azimuthInDegress;
-//        }
+            // create a rotation animation (reverse turn degree degrees)
+            RotateAnimation ra = new RotateAnimation(
+                    mEventDegree,
+                    -degree,
+                    Animation.RELATIVE_TO_SELF, 0.5f,
+                    Animation.RELATIVE_TO_SELF, 0.5f);
+            // how long the animation will take place
+            ra.setDuration(210);
+            // set the animation after the end of the reservation status
+            ra.setFillAfter(true);
+            // Start the animation
+            mPointer.startAnimation(ra);
+            mCurrentDegree = -degree;
+        }
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (user.lastY == 0 && user.lastX == 0) {
+            Toast.makeText(ActivityEventsList.this,
+                    "Your device is located", Toast.LENGTH_LONG).show();
+        }
+        user.lastY = location.getLongitude();
+        user.lastX = location.getLatitude();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
 
     }
 }
